463a3fd1cce98c6ad546bc13fed35a99d7328c36
qmoriceau <quentin.moriceau@ipilyon.net>
2020-10-08 10:46:24 +0200

    exos

--- src/main/java/com/ipiecoles/java/java220/Entreprise.java
+++ src/main/java/com/ipiecoles/java/java220/Entreprise.java
@@ -1,7 +1,7 @@
 package com.ipiecoles.java.java220;
 import org.joda.time.LocalDate;
 
-public class Entreprise{
+public class Entreprise {
     public static final Double SALAIRE_BASE = 1480.27;
     public static final Integer NB_CONGES_BASE = 25;
     public static final Double INDICE_MANAGER = 1.3;
